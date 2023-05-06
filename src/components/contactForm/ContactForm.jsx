f2c1bd615b05371333dc3b3b387b64c5a1406e4d
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-05-06 22:21:26 +0300

    Update ContactForm.jsx
    rewrite ContactForm

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,74 +1,71 @@
-import { Component } from 'react'; // імпорт базового класу React Component
+import { useState } from 'react';
 import css from './ContactForm.module.css'; // стилізація
 import PropTypes from 'prop-types'; // типізація
 
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export const ContactForm = ({ addContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  resetForm = () => {
-    this.setState({ name: '', number: '' });
-  };
-
-  // оновлення стану компонента
-  onChangeInput = evt => {
+  const onChangeInput = evt => {
     const { name, value } = evt.currentTarget;
-    this.setState({ [name]: value });
+    //this.setState({ [name]: value });
+    if (name === 'name') {
+      setName(value);
+    }
+    if (name === 'number') {
+      setNumber(value);
+    }
   };
-
-  render() {
-    return (
-      <>
-        <form
-          className={css.formstyle}
-          onSubmit={evt => {
-            evt.preventDefault();
-            this.props.addContact(this.state);
-            this.resetForm();
-          }}
-        >
-          <label className={css.label}>
-            Name
-            <br />
-            <input
-              className={css.input}
-              onChange={this.onChangeInput}
-              value={this.state.name}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
+  return (
+    <>
+      <form
+        className={css.formstyle}
+        onSubmit={evt => {
+          evt.preventDefault();
+          addContact({ name, number });
+          //this.resetForm();
+          setName('');
+          setNumber('');
+        }}
+      >
+        <label className={css.label}>
+          Name
           <br />
-          <label htmlFor="">
-            Number
-            <br />
-            <input
-              className={css.input}
-              onChange={this.onChangeInput}
-              value={this.state.number}
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </label>
+          <input
+            className={css.input}
+            onChange={onChangeInput}
+            value={name}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <br />
+        <label htmlFor="">
+          Number
           <br />
-          <button className={css.button} type="submit">
-            Add contact
-          </button>
-        </form>
-      </>
-    );
-  }
-}
+          <input
+            className={css.input}
+            onChange={onChangeInput}
+            value={number}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <br />
+        <button className={css.button} type="submit">
+          Add contact
+        </button>
+      </form>
+    </>
+  );
+};
 
-// типізація
 ContactForm.propTypes = {
   addContact: PropTypes.func.isRequired,
 };
